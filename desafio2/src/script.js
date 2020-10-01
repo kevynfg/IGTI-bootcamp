36c7ba1cf43ad1cd01e0228ef5d4951c9a7ce16d
kevyn <kevynredlotus@gmail.com>
2020-10-01 17:43:24 -0300

    - update

--- desafio2/src/script.js
+++ desafio2/src/script.js
@@ -1,12 +1,12 @@
 let infoSearch = document.querySelector('.info');
 let itemsSearch = document.querySelector('.items');
-let allPeople = [];
-let novoUser = [];
-let contagemFem = [];
-let contagemMasc = [];
-let contagemPessoas = null;
 let buttonSearch = document.querySelector('#buttonSearch');
 let inputSearch = document.querySelector('#search');
+let allPeople = [];
+let newPeople = [];
+let femaleCount = [];
+let maleCount = [];
+let peopleCount = null;
 
 window.addEventListener('load', () => {
   Fetching();
@@ -32,9 +32,9 @@ async function Fetching() {
   allPeople.forEach((user) => {
     const { gender } = user;
     if (gender == 'female') {
-      contagemFem = [...contagemFem, gender];
+      femaleCount = [...femaleCount, gender];
     } else if (gender == 'male') {
-      contagemMasc = [...contagemMasc, gender];
+      maleCount = [...maleCount, gender];
     }
   });
 
@@ -42,7 +42,7 @@ async function Fetching() {
     return accumulator + current.dob;
   }, 0);
 
-  showGender(contagemMasc.length, contagemFem.length);
+  showGender(maleCount.length, femaleCount.length);
   somaIdades(somaIdade);
   mediaIdades(somaIdade / allPeople.length);
   renderPeople(allPeople);
@@ -52,7 +52,7 @@ const renderPeople = (peoples) => {
   itemsSearch.innerHTML = '';
   let output = '';
   peoples.forEach((people) => {
-    contagemPessoas++;
+    peopleCount++;
     const { name, picture, dob } = people;
     output += `
         <tr>
@@ -60,46 +60,46 @@ const renderPeople = (peoples) => {
         </tr>
         `;
   });
-  contagemPessoas = peoples.length;
-  itemsSearch.innerHTML = `<tr><th>${contagemPessoas} Usuário(s) foram encontrado(s)</th></tr>`;
+  peopleCount = peoples.length;
+  itemsSearch.innerHTML = `<tr><th>${peopleCount} Usuário(s) foram encontrado(s)</th></tr>`;
   itemsSearch.innerHTML += output;
 };
 
 inputSearch.addEventListener('keyup', (event) => {
-  contagemFem = [];
-  contagemMasc = [];
+  femaleCount = [];
+  maleCount = [];
   if (event.keyCode === 13) {
-    contagemPessoas--;
+    peopleCount--;
     event.preventDefault();
     const element = event.target.value.toLowerCase();
-    novoUser = allPeople
+    newPeople = allPeople
       .filter((name) => name.name.toLowerCase().includes(element))
       .sort((a, b) => {
         return a.name.localeCompare(b.name);
       });
 
-    novoUser.forEach((user) => {
+    newPeople.forEach((user) => {
       const { gender } = user;
       if (gender == 'female') {
-        contagemFem = [...contagemFem, gender];
+        femaleCount = [...femaleCount, gender];
       } else if (gender == 'male') {
-        contagemMasc = [...contagemMasc, gender];
+        maleCount = [...maleCount, gender];
       }
     });
 
-    const somaIdade = novoUser.reduce((accumulator, current) => {
+    const somaIdade = newPeople.reduce((accumulator, current) => {
       return accumulator + current.dob;
     }, 0);
 
-    showGender(contagemMasc.length, contagemFem.length);
+    showGender(maleCount.length, femaleCount.length);
     somaIdades(somaIdade);
-    mediaIdades(somaIdade / novoUser.length);
+    mediaIdades(somaIdade / newPeople.length);
 
-    if (novoUser.length === 0) {
+    if (newPeople.length === 0) {
       infoSearch.innerHTML = 'Não há estatísticas';
-      renderPeople(novoUser);
+      renderPeople(newPeople);
     } else {
-      renderPeople(novoUser);
+      renderPeople(newPeople);
     }
   }
 });
@@ -126,37 +126,61 @@ function mediaIdades(idades) {
 }
 
 function buttonEvent() {
-  contagemFem = [];
-  contagemMasc = [];
-  contagemPessoas--;
+  femaleCount = [];
+  maleCount = [];
+  peopleCount--;
   const element = inputSearch.value.toLowerCase();
-  novoUser = allPeople
+  newPeople = allPeople
     .filter((name) => name.name.toLowerCase().includes(element))
     .sort((a, b) => {
       return a.name.localeCompare(b.name);
     });
 
-  novoUser.forEach((user) => {
+  newPeople.forEach((user) => {
     const { gender } = user;
     if (gender == 'female') {
-      contagemFem = [...contagemFem, gender];
+      femaleCount = [...femaleCount, gender];
     } else if (gender == 'male') {
-      contagemMasc = [...contagemMasc, gender];
+      maleCount = [...maleCount, gender];
     }
   });
 
-  const somaIdade = novoUser.reduce((accumulator, current) => {
+  const somaIdade = newPeople.reduce((accumulator, current) => {
     return accumulator + current.dob;
   }, 0);
 
-  showGender(contagemMasc.length, contagemFem.length);
+  showGender(maleCount.length, femaleCount.length);
   somaIdades(somaIdade);
-  mediaIdades(somaIdade / novoUser.length);
+  mediaIdades(somaIdade / newPeople.length);
 
-  if (novoUser.length === 0) {
+  if (newPeople.length === 0) {
     infoSearch.innerHTML = 'Não há estatísticas';
-    renderPeople(novoUser);
+    renderPeople(newPeople);
   } else {
-    renderPeople(novoUser);
+    renderPeople(newPeople);
   }
 }
+
+inputSearch.addEventListener('keyup', () => {
+  femaleCount = [];
+  maleCount = [];
+  if (inputSearch.value === '') {
+    const somaIdade = allPeople.reduce((accumulator, current) => {
+      return accumulator + current.dob;
+    }, 0);
+
+    allPeople.forEach((user) => {
+      const { gender } = user;
+      if (gender == 'female') {
+        femaleCount = [...femaleCount, gender];
+      } else if (gender == 'male') {
+        maleCount = [...maleCount, gender];
+      }
+    });
+
+    showGender(maleCount.length, femaleCount.length);
+    renderPeople(allPeople);
+    somaIdades(somaIdade);
+    mediaIdades(somaIdade / allPeople.length);
+  }
+});
