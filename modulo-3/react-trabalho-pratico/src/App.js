7987f4772fb8277c0c09435bc2675afd2432d81f
kevyn <kevynredlotus@gmail.com>
2020-10-22 14:37:41 -0300

    - modulo-3 study

--- modulo-3/react-trabalho-pratico/src/App.js
+++ modulo-3/react-trabalho-pratico/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { calculateSalaryFrom } from '../src/components/helpers/salary';
-import inputSalary from './components/inputs/inputSalary';
+import InputReadOnly from './components/inputs/InputReadOnly';
+import InputSalary from './components/inputs/InputSalary';
+import ProgressBarSalary from './components/ProgressBarSalary/ProgressBarSalary';
 
 export default class App extends Component {
   constructor() {
@@ -13,19 +15,73 @@ export default class App extends Component {
   }
 
   componentDidMount() {
+    const { fullSalary } = this.state
+    this.setState({
+      results: calculateSalaryFrom(fullSalary)
+    })
+    console.log('Cálculo de salário efetuado!', this)
+  }
 
+  handleChangeSalary = (fullSalary) => {
+    this.setState({
+      fullSalary: fullSalary,
+      results: calculateSalaryFrom(fullSalary)
+    })
   }
 
 
   render() {
-    const { fullSalary } = this.state
+
+    const { fullSalary,
+      results: { baseINSS, discountINSS, baseIRPF, discountIRPF, netSalary } } = this.state
+    const bar1 = (discountINSS / baseINSS) * 100;
+    const bar2 = (discountIRPF / baseINSS) * 100;
+    const bar3 = 100 - (bar1 + bar2);
 
     return (
       <div className="container">
-        <h1 className="center-align">Hello world</h1>
+        <h3 className="center-align">Cálculo de Salário</h3>
         <div className="row">
           <div className="col s12">
-            <inputSalary></inputSalary>
+            <InputSalary
+              value={fullSalary}
+              onChangeSalary={this.handleChangeSalary} />
+
+            <InputReadOnly
+              id="1"
+              value={baseINSS}
+              label="Base INSS"
+            />
+            <InputReadOnly
+              id="2"
+              value={discountINSS}
+              label="Desconto INSS"
+              color="#e67e22"
+            />
+            <InputReadOnly
+              id="3"
+              value={baseIRPF}
+              label="Base IRPF"
+            />
+            <InputReadOnly
+              id="4"
+              value={discountIRPF}
+              label="Desconto IRPF"
+              color="#c0392b"
+            />
+            <InputReadOnly
+              id="5"
+              value={netSalary}
+              label="Salário líquido"
+              color="#16a085"
+            />
+            <div className="row">
+              <ProgressBarSalary
+                bar1={bar1}
+                bar2={bar2}
+                bar3={bar3}
+              />
+            </div>
           </div>
         </div>
       </div >
--- modulo-3/react-trabalho-pratico/src/components/inputs/inputSalary.js
+++ modulo-3/react-trabalho-pratico/src/components/inputs/inputSalary.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export default class inputSalary extends Component {
+export default class InputSalary extends Component {
     handleChange = (event) => {
         const newValue = event.target.value
 
@@ -12,9 +12,15 @@ export default class inputSalary extends Component {
             <div className="input-field col s12">
                 <input
                     type="number"
+                    min="0"
+                    max="999999999"
+                    step="100"
+                    id="fullSalary"
+                    style={{ fontSize: '1.4rem', fontWeight: 'bold' }}
                     onChange={this.handleChange}
-                    value={value} >test</input>
-
+                    value={value}
+                />
+                <label className="active" htmlFor="fullSalary">Salário bruto</label>
             </div>
         )
     }
